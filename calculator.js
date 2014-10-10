7cb2bd588e2cd7472cd59f6182387660647e9084
NeLk <nelk42@gmail.com>
2014-10-10 16:26:19 +0100

    handling no string, empty string, single number & two numbers

--- calculator.js
+++ calculator.js
@@ -1,3 +1,17 @@
-exports.add = function(expression){
-	return parseInt(expression || 0)
+exports.add = function(text){
+	var numbers = getNums(text)
+	var sum = getSum(numbers)
+	return sum
 };
+
+getNums = function(text){
+	return text.split(',')	
+}
+
+getSum = function(numbers){
+	var sum = 0
+	for (var i=0; i<numbers.length; i++){
+		sum += parseInt(numbers[i])
+	}
+	return (sum || 0)
+}
--- specs/calculator-spec.js
+++ specs/calculator-spec.js
@@ -1,31 +1,29 @@
 var calculator = require('../calculator')
 
 describe('As a math student', function() {
-	describe('when I use the string calculator', function() {
-
+	describe('the string calculator', function() {
 		beforeEach(function(){
 
 		})
 
-		it('should return 0 for an empty string', function() {
+		it('should return 0 if there is no value', function() {
 			var result = calculator.add('')
 			expect(result).toBe(0);
 		});
 
-		it('should return n for a given n number', function() {
-			var result = calculator.add('1')
-			expect(result).toBe(1)
+		it('should return 0 if the parameter is an empty string', function() {
+			var result = calculator.add('')
+			expect(result).toBe(0);
+		});
+
+		it('should return n if the parameter is a given n number', function() {
+			var result = calculator.add('42')
+			expect(result).toBe(42)
+		})
+
+		it('should return sum(a,b) if numbers "a,b" are given as parameter', function() {
+			var result = calculator.add('42,21')
+			expect(result).toBe(63)
 		})
 	});
 });
-
-/* 1) Create a simple String calculator with a method int Add(string numbers)
-
-	The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
-
-	Start with the simplest test case of an empty string and move to 1 and two numbers
-
-	Remember to solve things as simply as possible so that you force yourself to write tests you did not think about
-
-	Remember to refactor after each passing test 
-*/
